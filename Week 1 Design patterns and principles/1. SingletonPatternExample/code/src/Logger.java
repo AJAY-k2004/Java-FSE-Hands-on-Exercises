66f249b97b9f9cc1ff64fff2c32ec3edd99a5367
AJAY-k2004 <ajaykajaycs@gmail.com>
2025-06-21 18:01:17 +0530

    update

--- Week 1 Design patterns and principles/1. SingletonPatternExample/code/src/Logger.java
+++ Week 1 Design patterns and principles/1. SingletonPatternExample/code/src/Logger.java
@@ -1,7 +1,7 @@
 
 public class Logger {
 
-    private static volatile Logger instance;
+    private static Logger instance;
 
     private Logger() {
         System.out.println("Logger initialized.");
@@ -9,11 +9,7 @@ public class Logger {
 
     public static Logger getInstance() {
         if (instance == null) {
-            synchronized (Logger.class) {
-                if (instance == null) {
-                    instance = new Logger();
-                }
-            }
+            instance = new Logger();
         }
         return instance;
     }
